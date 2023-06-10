ec7e8d07ff7be218e14b3f7d26cc4423831ea6aa
Myron R Schippers Jr <dubtwee@gmail.com>
2023-06-10 01:00:31 -0400

    adding new profile pictures to user data

--- src/data/users.ts
+++ src/data/users.ts
@@ -4,26 +4,22 @@ const USERS: IUser[] = [
   {
     id: 'mike',
     name: 'Mike',
-    image:
-      'https://i.picsum.photos/id/92/200/200.jpg?hmac=2cxZLFe94hVFQL5AERTDzRKET_GDG-2qpFi5-ctPekg',
+    image: '/assets/images/profile-avatar-1.jpg',
   },
   {
     id: 'mary1',
     name: 'Mary',
-    image:
-      'https://i.picsum.photos/id/646/200/200.jpg?hmac=3jbia15y-hA5gmqVJjmk6BPJiisi4j-fNKPi3iXRiRo',
+    image: '/assets/images/profile-avatar-5.jpg',
   },
   {
     id: 'jake',
     name: 'Jake',
-    image:
-      'https://i.picsum.photos/id/124/200/200.jpg?hmac=FuA4HgovVpaMlT_5gnjY_28jYCrrA2xrYXy3mJ9XDEw',
+    image: '/assets/images/profile-avatar-6.jpg',
   },
   {
     id: 'joe',
     name: 'Joe',
-    image:
-      'https://i.picsum.photos/id/703/200/200.jpg?hmac=6zWxIBRmIf2e0jZTqvKBIwrc7wm-dPkvGky4go6Yyvg',
+    image: '/assets/images/profile-avatar-3.jpg',
   },
 ];
 
